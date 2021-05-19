f87b70667a6ea981603b15bcf93357c399bf4013
Jeevan Kumar Papagari <jeevankumar922@gmail.com>
2021-05-19 21:54:36 +0530

    Leap years
    Write a function that given an array of some size, with its elements being years, returns an array of strings of either "yes" or "no" where the ith string is "yes" if the ith year is a leap year and "no" otherwise.
    
    Note that you can assume every year that is divisible by 4 is a leap year.
    
    Sample Input 0
    
    5
    1400
    1979
    1996
    2016
    2017
    Sample Output 0
    
    yes
    no
    yes
    yes
    no

---
+++ Leap Years.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+char** leapyears (int arr[], int size)
+{
+    int i;
+	char **result = malloc(sizeof(char*) * size);
+    for(i=0;i<size;i++)
+        result[i] = malloc(4);
+	char yes[] = "yes";
+	char no[] = "no";
+
+	for (i = 0; i < size; i++)
+	{
+		if (arr[i]%4==0) strcpy(result[i], yes); //replace the '_' with proper condition
+		else strcpy(result[i], no);
+	}
+	return result;
+}	
+int main()
+{
+    int i, N;
+    scanf("%d", &N);
+    int arr[N];
+    for (i = 0; i < N; i++) scanf("%d", &arr[i]);
+    char** ans = leapyears(arr, N);
+    for (i = 0; i < N; i++) puts(ans[i]);
+    return 0;
+}
