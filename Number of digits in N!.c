525bc6fabddc980abac76cdcfe7129f3e8855ab3
Jeevan Kumar Papagari <jeevankumar922@gmail.com>
2021-05-24 15:54:44 +0530

    Number of Digits in N!
    As simple as the title, given a number N, print the number of digits in N!
    
    N! is defined as : N! = 1*2*3...(N-1)*N
    
    0! = 0 and 1! = 1.
    
    No number ever contains any leading zeros.
    
    Input Format
    
    Input contains only one number, N.
    
    Constraints
    
    1 <= N <= 1000
    
    Output Format
    
    Output one number that is equal to the number of digits in N!
    
    Sample Input 0
    
    6
    Sample Output 0
    
    3
    Explanation 0
    
    6! = 1x2x3x4x5x6 = 720 which has 3 digits. So the answer is 3.

--- Number of digits in N!.c
+++ Number of digits in N!.c
@@ -1,35 +1,3 @@
-As simple as the title, given a number N, print the number of digits in N!
-
-N! is defined as : N! = 1*2*3...(N-1)*N
-
-0! = 0 and 1! = 1.
-
-No number ever contains any leading zeros.
-
-Input Format
-
-Input contains only one number, N.
-
-Constraints
-
-1 <= N <= 1000
-
-Output Format
-
-Output one number that is equal to the number of digits in N!
-
-Sample Input 0
-
-6
-Sample Output 0
-
-3
-Explanation 0
-
-6! = 1x2x3x4x5x6 = 720 which has 3 digits. So the answer is 3.
-
-Code:
-
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
