c36cb9aa7e6d30360394553b18cafbc0b298c5f4
Jeevan Kumar Papagari <jeevankumar922@gmail.com>
2021-05-24 16:02:52 +0530

    Looped List
    You are not allowed to make any changes to the code other than the find_loop function body.
    
    In the function, you will be given a head node to a linked list.
    
    Two types of linked lists are :
    Terminating Lists :
    
    1 -> 2 -> 7 -> 8 -> 9 -> NULL
    Non-Terminating Lists(Loop in the list) :
    
    1 -> 2 -> 5 -> 15 -> 7--------
    	 ^	 	     |
    	 |                   |
    	 ---- 3 <- 10 <- 9<--
    In the second type of list, you can traverse forever and never reach the end.
    
    Modify the find_loop function so that given the head of the linked list, it returns 1 if the list has a loop and 0 otherwise.
    
    Your code's memory consumption must not be dependant on the size of the linked list.
    
    Input Format
    
    Not Needed
    
    Constraints
    
    None
    
    Output Format
    
    Not Needed

---
+++ O M01 - Looped List.c
@@ -0,0 +1,48 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "math.h"
+#include "string.h"
+
+struct Node{
+	int value;
+	struct Node* next;
+};
+typedef struct Node Node;
+Node links[105];
+
+void add_link(int from, int to){
+	links[from].next = &links[to];
+}
+void set_value(int index, int val){
+	links[index].value = val;
+}
+
+int find_loop(Node* head)
+{
+	Node *ptr = head, *fwdptr = head;
+    while(ptr && fwdptr && fwdptr->next)
+    {
+        ptr = ptr->next;
+        fwdptr = fwdptr->next->next;
+        if(ptr == fwdptr)
+            return 1;
+    }
+    return 0;
+	
+}
+
+int main(){
+	int i,n,m,tmp,tmp1,tmp2;
+	scanf("%d %d", &n, &m);
+	for(i = 0; i < n; i++){
+		scanf("%d", &tmp);
+		set_value(i, tmp);
+	}
+	for(i = 0; i < m; i++){
+		scanf("%d %d", &tmp1, &tmp2);
+		add_link(tmp1,tmp2);
+	}
+	if(find_loop(&links[0]) == 1) printf("Loop Found");
+	else printf("No Loop Found");
+	return 0;
+}
