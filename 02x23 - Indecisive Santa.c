172c9b49c0ca52a9aa27b4a9722c7c401ae19bd7
Jeevan Kumar Papagari <48345440+jeevankumarpapagari@users.noreply.github.com>
2021-11-11 21:59:11 +0530

    Update 02x23 - Indecisive Santa.c

--- 02x23 - Indecisive Santa.c
+++ 02x23 - Indecisive Santa.c
@@ -1,38 +1,27 @@
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-#include <stdlib.h>
-int a[201][201],n,m,g11,g12,g21,g22;
-int min1,min2;
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
 int main() 
 {
-    scanf("%d%d%d%d%d%d",&n,&m,&g11,&g12,&g21,&g22);
-    int k=0,x,y;
-    for(int i=1;i<=n;i++)
+    int n,m,g11,g12,g21,g22;
+    scanf("%d %d %d %d %d %d",&n,&m,&g11,&g12,&g21,&g22);
+    
+    int i, j, min1, min2, result = 0;
+    
+    for(i=1;i<=n;i++)
     {
-        for(int j=1;j<=m;j++)
+        for(j=1;j<=m;j++)
         {
-           if(i<g11)    x=g11-i;
-           
-           else         x=i-g11;
-          
-           if(j<g12)    y=g12-j;
-
-           else y=j-g12;
-           
-           min1=x+y;
-           
-           if(i<g21)    x=g21-i;
-           
-           else         x=i-g21;
-          
-           if(j<g22)    y=g22-j;
-
-           else y=j-g22;
-           min2=x+y;
-           if(min1==min2)k++;
+            min1 = (i<g11 ? g11-i : i-g11) + (j<g12 ? g12-j : j-g12);
+            min2 = (i<g21 ? g21-i : i-g21) + (j<g22 ? g22-j : j-g22);
+            result = min1 == min2 ? result+1 : result;
         }
     }
-    printf("%d",k);
+    printf("%d",result);
     return 0;
 }
