466e759a79475cb5b961fd791bfef6f10c9ccb9a
Jeevan Kumar Papagari <48345440+jeevankumarpapagari@users.noreply.github.com>
2021-11-11 20:56:41 +0530

    Update Q 801 - Zig Zag Traversal.c

--- Q 801 - Zig Zag Traversal.c
+++ Q 801 - Zig Zag Traversal.c
@@ -2,60 +2,32 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
-
-int m[1000000][2] = {0};
-int st1[1000000], st2[1000000];
-int top1 = -1,top2 = -1, x;
-
-int pop1()
-{
-    return st1[top1--];
-}
-
-int pop2()
-{
-    return st2[top2--];
-}
-
-int main() 
-{
-    int n,i;
-    scanf("%d",&n);
-    
-    int head, child;
-    char pos;
-    
-    for(i=0;i<n-1;i++)
+int t[1000000][2],s1[100000],s2[10000],t1=-1,t2=-1;
+int main() {
+    int n; scanf("%d",&n);
+    int x,y;char c;
+    for(int i=0;i<n-1;i++)
     {
-        scanf("%d%d %c",&head,&child,&pos);
-        if(pos == 'L')      m[head][0] = child;
-        else                m[head][1] = child;
+        scanf("%d %d %c",&x,&y,&c);
+        if(c=='L')t[x][0]=y;
+        else t[x][1]=y;
     }
-    
-    st1[++top1] = 1;
-    while(top1!=-1 || top2!=-1)
-    {
-        while(top1 >= 0)
-        {
-            x = pop1();
-            if(x!=0)
-            {
-                printf("%d ",x);
-                st2[++top2] = m[x][0];
-                st2[++top2] = m[x][1];
-            }
-        }
-        while(top2 >= 0)
-        {
-            x = pop2();
-            if(x!=0)
-            {
-                printf("%d ",x);
-                st1[++top1] = m[x][1];
-                st1[++top1] = m[x][0];
-            }
-        }
+    s1[++t1]=1;
+    while(t1!=-1||t2!=-1){
+       while(t1!=-1)
+       {
+           int x=s1[t1--];
+           printf("%d ",x);
+           if(t[x][0])s2[++t2]=t[x][0];
+           if(t[x][1])s2[++t2]=t[x][1];
+       }
+       while(t2!=-1)
+       {
+           int x=s2[t2--];
+           printf("%d ",x);
+           if(t[x][1])s1[++t1]=t[x][1];
+           if(t[x][0])s1[++t1]=t[x][0];
+       }
     }
-    
     return 0;
 }
