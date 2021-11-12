ad77cc24b3b90fed76b5be6822385d01792d865d
Jeevan Kumar Papagari <48345440+jeevankumarpapagari@users.noreply.github.com>
2021-11-12 13:43:53 +0530

    Update G M03 - Validate Sudoku.c

--- G M03 - Validate Sudoku.c
+++ G M03 - Validate Sudoku.c
@@ -1,82 +1,72 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <algorithm>
-using namespace std;
+#include<stdio.h>
+#include<stdlib.h>
+
+int compare(const void *x,const void *y)
+{
+    return *(int*)x - *(int*)y;
+}
 
 
-int main() {
-    long long int t,i,j,k,sum,flag;
-    cin>>t;
-    for(i=0;i<t;i++)
+int main()
+{
+    int tc;
+    scanf("%d",&tc);
+    
+    while(tc--)
     {
-        int a[10][10];
-        flag=0;
-        for(j=0;j<9;j++)
+        int sudoku[9][9], flag = 1;
+        int i, j;
+        for(i=0;i<9;i++)
         {
-            sum=45;
-            int b[10];
-            for(k=0;k<9;k++)
-            {
-                cin>>a[j][k];
-                sum=sum-a[j][k];
-                b[k]=a[j][k];
-            }
-            if(sum!=0)
-            {
-                flag=1;
-                break;
-            }
-            sort(b,b+9);
-            if(b[0]==1 && b[1]==2 && b[2]==3 && b[3]==4 && b[4]==5 && b[5]==6 && b[6]==7 && b[7]==8 && b[8]==9)
-            {
-                
-            }
-            else
-            {
-                flag=1;
-                break;
-            }
-        }
-        if(flag==1)
-            cout<<"inValid"<<endl;
-        else
-        {
+            int sum = 45, t[9];
             for(j=0;j<9;j++)
             {
-                int b[10],s[10];
-                sum=45;
-                for(k=0;k<9;k++)
-                {
-                    b[k]=a[k][j];
-                    sum=sum-a[k][j];
-                    s[k] = a[(j / 3) * 3 + k / 3][j * 3 % 9 + k % 3];
-
-                }
-                if(sum!=0)
-                {
-                    flag=1;
-                    break;
-                }
-                sort(b,b+9);
-                sort(s,s+9);
-            if(b[0]==1 && b[1]==2 && b[2]==3 && b[3]==4 && b[4]==5 && b[5]==6 && b[6]==7 && b[7]==8 && b[8]==9 && s[0]==1 && s[1]==2 && s[2]==3 && s[3]==4 && s[4]==5 && s[5]==6 && s[6]==7 && s[7]==8 && s[8]==9 )
-            {
-                
+                scanf("%d",&sudoku[i][j]);
+                sum -= sudoku[i][j];
+                t[j] = sudoku[i][j];
             }
-            else
+            if(sum)
             {
-                flag=1;
+                flag = 0;
                 break;
             }
-            }
-            if(flag==1)
-                cout<<"inValid"<<endl;
+            qsort(t,9,sizeof(int),compare);
+            if(t[0]==1 && t[1]==2 && t[2]==3 && t[3]==4 && t[4]==5 && t[5]==6 && t[6]==7 && t[7]==8 && t[8]==9);
             else
-                cout<<"Valid"<<endl;
+            {
+                flag = 0;
+                break;
+            }
         }
-      
+        if(flag == 0)
+            printf("inValid\n");
+        else
+        {
+            for(i=0;i<9;i++)
+            {
+                int sum = 45, t[9], c[9];
+                for(j=0;j<9;j++)
+                {
+                    t[j] = sudoku[j][i];
+                    sum = sum - sudoku[j][i];
+                    c[j] = sudoku[(i / 3) * 3 + j / 3][i * 3 % 9 + j % 3];
+                }
+                if(sum)
+                {
+                    flag = 0;
+                    break;
+                }
+                qsort(t,9,sizeof(int),compare);
+                qsort(c,9,sizeof(int),compare);
+                if(t[0]==1 && t[1]==2 && t[2]==3 && t[3]==4 && t[4]==5 && t[5]==6 && t[6]==7 && t[7]==8 && t[8]==9 && c[0]==1 && c[1]==2 && c[2]==3 && c[3]==4 && c[4]==5 && c[5]==6 && c[6]==7 && c[7]==8 && c[8]==9);
+                else
+                {
+                    flag = 0;
+                    break;
+                }
+            }
+            flag ? printf("Valid\n") : printf("inValid\n");
+        }
     }
     return 0;
 }
